5b77e437c987f3ccb970fd0ce61ea51676c2f353
Cesar Tovar <cjataonline@gmail.com>
2024-05-09 09:03:31 -0400

    sensors_plus package agregado y gyroscope_provider y screen en funcionamiento

--- lib/presentation/screens/sensors/gyroscope_screen.dart
+++ lib/presentation/screens/sensors/gyroscope_screen.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:miscelaneus/presentation/providers/providers.dart';
 
-class GyroscopeScreen extends StatelessWidget {
+class GyroscopeScreen extends ConsumerWidget {
   const GyroscopeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+
+    final gyroscope$ = ref.watch(gyroscopeProvider);
+    
     return Scaffold(
       appBar: AppBar(
         title: const Text('Giroscopio'),
       ),
       body: Center(
-        child: Text('hola'),
+        child: gyroscope$.when(
+          data: (value) => Text(value.toString(),
+          style: const TextStyle(fontSize: 30),),
+           error: (error, stackTrace) => Text('$error'),
+            loading: () => const CircularProgressIndicator(strokeWidth: 2,))
       ),
     );
   }
