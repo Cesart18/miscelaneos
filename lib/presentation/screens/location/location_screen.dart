ed9bf5cc9665c21752c6625d63b82b05bfa59dd3
Cesar Tovar <cjataonline@gmail.com>
2024-05-19 18:59:42 -0400

    pantalla de ubicacion agregada correctamente

--- lib/presentation/screens/location/location_screen.dart
+++ lib/presentation/screens/location/location_screen.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:miscelaneus/presentation/providers/providers.dart';
 
-class LocationScreen extends StatelessWidget {
+class LocationScreen extends ConsumerWidget {
   const LocationScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+
+    final userLocationAsync = ref.watch(userLocationProvider);
+    final watchLocation$ = ref.watch(watchLocationProvider);
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Location Screen'),
+        title: const Text('Ubicacion'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // ! current Location
+            const Text('Ubicacion Actual'),
+            userLocationAsync.when(
+              data: (data) => Text('$data'), 
+              error: (error, stackTrace) => Text('$error'), 
+              loading: () => const CircularProgressIndicator(strokeWidth: 2,)
+              ),
+            const SizedBox(height: 30,),
+            // ! current location
+            const Text('Seguimiento de ubicacion'),
+
+            watchLocation$.when(
+              data: (data) => Text('$data'),
+               error: (error, stackTrace) => Text('$error'),
+                loading: () => const CircularProgressIndicator(strokeWidth: 2,)
+                )
+          ],
+        ),
       ),
     );
   }
